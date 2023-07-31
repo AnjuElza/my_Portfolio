33539f168abb1a90695c51f728984c66772ca962
AnjuElza <anjuelz@gmail.com>
2023-07-31 12:13:04 +0530

    updated about

--- src/components/About.js
+++ src/components/About.js
@@ -25,24 +25,30 @@ function About(){
        </Box>
        
             <Paper elevation={3} 
-                sx={{ width: '40%', height: 'fit-content', ml: 15, mt: 5, padding:3, mb:3, fontSize:18, display: 'flex', flexDirection: 'column', alignItems: 'center'  }}>
+                sx={{ width: '40%', height: 'fit-content', ml: 15, mt: 5, padding:3, mb:3, fontSize:18, display: 'flex', 
+                flexDirection: 'column', alignItems: 'center', textAlign: 'justify'  }}>
             <><p>
                
-                I am a fullstack MERN developer, also interested in Frontend React developer jobs. My aim is to develop reusable, testable and 
-                efficient codes that help in meeting user expectations. I love coding and I am passionate about my job.
+                I am a motivated and detail-oriented fullstack MERN developer, also interested in Frontend React developer jobs. 
+                My aim is to develop reusable, testable and 
+                responsive web designs that help in meeting user expectations. 
             </p><p>
                     
                     In this fast moving industry, I am excited to constantly learn and improve my skills.
-                    I am looking forward to work in a company where I will be able to contribute and also upskill myself.
+                    I am seeking a challenging role  where I can 
+                utilize my expertise in HTML, CSS, JavaScript, and my proficiency in frameworks such as React and Node.js and
+                 my problem-solving capabilities to develop high-performing web applications. Committed to delivering high-quality code, 
+                 staying updated with emerging technologies, and collaborating effectively in cross-functional teams to achieve project goals.
                 </p>
+                
                 <p>
                     Check out my projects and feel free to contact me if you have a suitable job matching my skills.
                 </p>
-                <Button variant="contained" sx={{ mx: 2, color: 'black', mt:3, mb:3, justifyContent: 'center' }}>
+                {/* <Button variant="contained" sx={{ mx: 2, color: 'black', mt:3, mb:3, justifyContent: 'center' }}>
                       <Link to="projects" smooth={true} duration={500} offset={-90}>
                         My Projects
                         </Link>
-                        </Button>
+                        </Button> */}
                 </>
         </Paper>
        
